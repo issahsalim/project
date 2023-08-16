b949e231fc66fff8381d324a77c7192878654340
issahsalim <130291719+issahsalim@users.noreply.github.com>
2023-08-16 08:10:51 +0000

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include<string>
+#include<sstream>
+using namespace std;
+int main()
+{
+	/*
+	int number;
+	string value;
+ 	
+	cout<<"Enter number: ";
+	cin>>value;
+	
+	stringstream ss(value);
+	if(ss>>number && ss.eof()){
+		cout<<"You entered:"<<number;
+	}
+	else{
+		cout<<"invalid try again";
+	}
+	*/
+	
+  int 
+} 
+
